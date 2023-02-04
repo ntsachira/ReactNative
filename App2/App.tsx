3aba7da2a89dbaa64c6541f618b7acd96e2509f0
ntsachira <sct20090@std.uwu.ac.lk>
2023-02-04 07:23:10 +0530

    page navigation day 1

--- App2/App.tsx
+++ App2/App.tsx
@@ -1,10 +1,62 @@
 import React from "react";
+import {Button, StyleSheet, Text, View,Alert} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import {Button} from "react-native";
+
+const Stack = createNativeStackNavigator();
 
 function App(){
- const b = <Button title="Create New Accunt"/> 
- return b; 
+ const ui = (
+   <NavigationContainer>
+    <Stack.Navigator>
+
+        <Stack.Screen name="Home" component={Home} options={{title:"Java Institute"}}/>
+        <Stack.Screen name="Profile" component={Profile} options={{title:"Profile"}}/>
+
+    </Stack.Navigator>
+   </NavigationContainer> 
+ );
+ return ui; 
 }
 
 export default App;
+
+function Home(navigation){
+    const ui=(
+        <View style={styles.main}>
+            <Text style={styles.text1}>Home Screen</Text>
+            <Button title="Go To Profile" onPress={m}/>
+        </View>
+    );
+
+    function m(){
+        //Alert.alert("","");
+        navigation.navigate("Profile");
+    }
+    return ui;
+}
+
+function Profile(navigation){
+    const ui=(
+        <View style={styles.main}>
+            <Text style={styles.text1}>Profile Screen</Text>
+        </View>
+    );
+
+    return ui;
+}
+
+const styles = StyleSheet.create(
+    {
+        main:{
+            flex:1,
+            justifyContent:"center",
+            alignItems:"center",
+        },
+        text1:{
+            fontSize:40,
+            fontWeight:"bold",
+        }
+    }
+);
